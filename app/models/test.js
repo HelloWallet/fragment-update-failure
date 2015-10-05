f6b8c93066bc668b1e6197aea1dc0fda7f207ee3
David Wolfand <david@hellowallet.com>
2015-10-05 10:09:31 -0400

    new issue where model fragments not correctly updating objects in child arrays

--- app/models/test.js
+++ app/models/test.js
@@ -2,6 +2,5 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   someAttr: DS.attr('string'),
-  anotherAttr: DS.attr('boolean'),
-  child: DS.hasOneFragment('foo')
+  theChildren: DS.hasManyFragments('foo')
 });
